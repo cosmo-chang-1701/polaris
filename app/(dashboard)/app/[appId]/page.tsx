033d052a0e2a0bb4039451b9c8b8cbcfe044351e
Cosmo Chang <cosmo.chang1701@gmail.com>
2024-05-28 17:26:52 +0800

    feat: added the POC of receive streaming text from local Ollama

--- app/(dashboard)/app/[appId]/page.tsx
+++ app/(dashboard)/app/[appId]/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import React, { FC, useState } from "react";
+import React, { FC, useState, useTransition, useEffect, useRef } from "react";
 import { SendHorizontal } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { cn, postAndStream } from "@/lib/utils";
 
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { useToast } from "@/components/ui/use-toast";
+import type { ToastProps } from "@/components/ui/toast";
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
 
@@ -11,8 +14,26 @@ import { useTranslation } from "@/app/i18n/client";
 
 const Page: FC = () => {
   const { t } = useTranslation("app-detail");
+  const { t: errorMessageT } = useTranslation("error-message");
+
+  const { toast } = useToast();
   const [prefixPropmt, setPrefixPrompt] = useState("");
   const [inputPrompt, setInputPrompt] = useState("");
+  const [responseMessages, setResponseMessages] = useState<string[]>([""]);
+  const [isPending, startTransition] = useTransition();
+
+  const responseSectionRef = useRef<HTMLInputElement>(null);
+  const bottomRef = useRef<HTMLInputElement>(null);
+
+  let scrollHeight = 0;
+  const responseSectionHeight = 630;
+  if (responseSectionRef.current)
+    scrollHeight = responseSectionRef.current.scrollHeight;
+  useEffect(() => {
+    if (bottomRef.current && scrollHeight > responseSectionHeight) {
+      bottomRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [scrollHeight]);
 
   function handleEnterPrefixPrompt(value: string) {
     setPrefixPrompt(value);
@@ -22,12 +43,51 @@ const Page: FC = () => {
     setInputPrompt(value);
   }
 
-  function handleSendMessage() {
-    console.log(prefixPropmt);
+  async function handleSendMessage() {
+    setInputPrompt("");
+    try {
+      await postAndStream(
+        "http://localhost:11434/api/chat",
+        {
+          model: "phi3",
+          stream: true,
+          messages: [
+            {
+              role: "system",
+              content: prefixPropmt
+            },
+            {
+              role: "user",
+              content: inputPrompt
+            }
+          ]
+        },
+        (chunk) => {
+          if (chunk.done) {
+            responseMessages.push("");
+          } else {
+            const index = responseMessages.length - 1;
+            responseMessages[index] =
+              responseMessages[index] + chunk.message.content;
+          }
+          startTransition(() => {
+            setResponseMessages(responseMessages);
+          });
+        }
+      );
+    } catch (e) {
+      const toastProps: ToastProps & { description?: React.ReactNode } = {
+        variant: "destructive",
+        title: errorMessageT("error"),
+        description: errorMessageT("unknownErrorOccurred")
+      };
+      if (e instanceof Error) toastProps.description = e.message;
+      toast(toastProps);
+    }
   }
 
   return (
-    <div className="flex h-screen flex-col md:flex-row">
+    <div className="flex flex-col md:flex-row">
       <div className="mr-4 md:w-1/2">
         <Textarea
           className="h-[400px]"
@@ -35,34 +95,60 @@ const Page: FC = () => {
           onChange={(e) => handleEnterPrefixPrompt(e.target.value)}
         />
       </div>
-      <div className="relative h-[700px] rounded-lg border md:w-1/2">
-        <div className="h-full">
-          <div className="h-[500px] overflow-y-auto"></div>
-          <div className="absolute bottom-5 w-full">
-            <div className="flex items-center px-5">
-              <Input
-                type="message"
-                placeholder={t("messageInputPlaceholder")}
-                onChange={(e) => handleInputPrompt(e.target.value)}
-                onKeyDown={(e) => {
-                  if (Object.is(e.key, "Enter")) handleSendMessage();
-                }}
-              />
-              <SendHorizontal
-                className={cn([
-                  "cursor-pointe absolute right-8 text-gray-400",
-                  {
-                    "cursor-pointer text-black": inputPrompt.length > 0
-                  }
-                ])}
-                onClick={handleSendMessage}
-              />
-            </div>
+      <div className="h-[700px] rounded-lg border md:w-1/2">
+        <div className="h-[630px]">
+          <div
+            ref={responseSectionRef}
+            className={`h-[${responseSectionHeight}px] overflow-y-auto px-5 py-4`}
+          >
+            {responseMessages.map((message, index) => {
+              if (message)
+                return <ResponseBlock key={index} message={message} />;
+            })}
+            <div ref={bottomRef}></div>
+          </div>
+        </div>
+        <div className="h-[70px] w-full py-3">
+          <div className="relative flex items-center px-5">
+            <Input
+              type="message"
+              value={inputPrompt}
+              placeholder={t("messageInputPlaceholder")}
+              onChange={(e) => handleInputPrompt(e.target.value)}
+              onKeyDown={(e) => {
+                if (Object.is(e.key, "Enter")) handleSendMessage();
+              }}
+            />
+            <SendHorizontal
+              className={cn([
+                "cursor-pointe absolute right-8 text-gray-400",
+                {
+                  "cursor-pointer text-black": inputPrompt.length > 0
+                }
+              ])}
+              onClick={handleSendMessage}
+            />
           </div>
         </div>
       </div>
     </div>
   );
 };
+
+function ResponseBlock({ message }: { message: string }) {
+  return (
+    <div className="mt-4">
+      <div className="flex items-start space-x-2">
+        <Avatar className="h-8 w-8">
+          <AvatarImage src="/ai.jpeg" alt="AI" />
+          <AvatarFallback className="bg-black">AI</AvatarFallback>
+        </Avatar>
+        <div className="w-full rounded-lg bg-gray-200 p-3 text-sm">
+          <p>{message}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default React.memo(Page);
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,45 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function postAndStream(
+  url: string,
+  data: object,
+  onChunkReceived: (chunk: {
+    message: { content: string };
+    done: boolean;
+  }) => void
+): Promise<void> {
+  const response = await fetch(url, {
+    body: JSON.stringify(data),
+    method: "POST"
+  });
+
+  // Checking if the response is not ok
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const reader = response.body?.getReader();
+  const decoder = new TextDecoder("utf-8");
+
+  // If the reader cannot be obtained, throw an error
+  if (!reader) {
+    throw new Error("Unable to get reader from response body");
+  }
+
+  let done = false;
+  // Reading the response stream in a loop until done
+  while (!done) {
+    const { done: doneReading, value } = await reader.read();
+    done = doneReading;
+    if (value) {
+      const chunk = decoder.decode(value, { stream: true });
+      // Process the chunk using the provided callback
+      onChunkReceived(JSON.parse(chunk));
+    }
+  }
 }
